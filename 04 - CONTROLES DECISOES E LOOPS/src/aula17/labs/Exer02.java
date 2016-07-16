fd1de2b97036c9767ab2654c012587d0f4250a99
José Malcher Jr <malcher.malch@gmail.com>
2016-07-16 10:02:18 -0300

    Arquivos iniciais dos Exercícios Sobre loops

---
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer02.java
@@ -0,0 +1,27 @@
+package aula17.labs;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		/*
+		 * Faça um programa que leia um nome de usuário e a sua senha e não
+		 * aceite a senha igual ao nome do usuário, mostrando uma mensagem de
+		 * erro e voltando a pedir as informações.
+		 */
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		boolean valido = true;
+		
+		do{
+			System.out.println("Entre com Login");
+			String nome = leitor.next();
+			System.out.println("Entre com a Senha");
+			String senha = leitor.next();
+		}
+
+	}
+
+}
