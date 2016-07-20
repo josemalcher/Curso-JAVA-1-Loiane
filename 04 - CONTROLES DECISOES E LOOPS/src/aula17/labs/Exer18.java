bef528e6c609dd90c8e0020241512b7dcfb20871
José Malcher Jr <malcher.malch@gmail.com>
2016-07-20 20:56:38 -0300

    Exercicio 17 - Faça um programa que peça um número inteiro e determine se ele é ou  não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer18.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer18.java
@@ -1,15 +1,35 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer18 {
 	/*
 	 * 18. Faça um programa que peça um número inteiro e determine se ele é ou
 	 * não um número primo. Um número primo é aquele que é divisível somente por
 	 * ele mesmo e por 1.
 	 */
+	//resolução loiane + 
 
 	public static void main(String[] args) {
 		
+		Scanner leitor = new Scanner(System.in);
 		
+		System.out.println("Entre com o numero");
+		
+		int numero = leitor.nextInt();
+		
+		boolean primo = true;
+		
+		for(int i= 2 ; i < numero; i++ ){
+			if(numero % i == 0){
+				System.out.println("Não é primo - É divisivel por: "+ i);
+				primo = false;
+			}
+		}
+		
+		if(primo){
+			System.out.println("É Primo: "+numero);
+		}
 		
 		
 		
