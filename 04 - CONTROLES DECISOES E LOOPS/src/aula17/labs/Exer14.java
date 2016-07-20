91b6efcc0b6293c14173560bd7a6e40584e15951
José Malcher Jr <malcher.malch@gmail.com>
2016-07-20 18:26:50 -0300

    Exercicio 14

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer14.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer14.java
@@ -1,5 +1,36 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer14 {
+	/*
+	 * 14 - Faça um programa que peça 10 números inteiros, calcule e mostre a
+	 * quantidade de números pares e a quantidade de números impares.
+	 */
+	// solução LOIANE
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+
+		int numero;
+		int pares = 0;
+		int impares = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("Digite um nunero inteiro");
+			numero = leitor.nextInt();
+
+			if (numero % 2 == 0) {
+				pares++;
+			} else {
+				impares++;
+			}
+
+		}
+		System.out.println("Pares "+pares);
+		System.out.println("Impares "+impares);
+
+	}
 
 }
