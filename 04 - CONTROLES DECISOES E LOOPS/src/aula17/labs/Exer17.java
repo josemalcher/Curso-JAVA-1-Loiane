076db1a8c5bffa7515e6df282427c8cd83818dcb
José Malcher Jr <malcher.malch@gmail.com>
2016-07-20 19:40:48 -0300

    Exercicio 17 - fatorial
    Faça um programa que calcule o fatorial de um número inteiro fornecido
    pelo usuário. Ex.: 5!=5.4.3.2.1=120

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer17.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer17.java
@@ -1,5 +1,31 @@
 package aula17.labs;
+import java.util.Scanner;
 
 public class Exer17 {
+	/*
+	 * Faça um programa que calcule o fatorial de um número inteiro fornecido
+	 * pelo usuário. Ex.: 5!=5.4.3.2.1=120
+	 */
+	//resolução Loiane com ajustes
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Entre com o numero : ");
+		int numero = leitor.nextInt();
+		
+		System.out.print(numero + "! = ");
+		
+		int fatorial = 1;
+		for (int i=numero; i>0; i--){
+			fatorial *= i;
+			System.out.printf("%s ",i);
+		}
+		System.out.println("\nResultado = "+ fatorial);
+		
+		
+		
+		
+	}
 
 }
