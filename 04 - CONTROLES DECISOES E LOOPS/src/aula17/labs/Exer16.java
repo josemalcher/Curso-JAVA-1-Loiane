b170ac90056466f2be1f68756f064d200bd26e2e
José Malcher Jr <malcher.malch@gmail.com>
2016-07-20 19:18:22 -0300

    Exercicio 16
    15. A série de Fibonacci é formada pela seqüência
    1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até
    o n−ésimo termo.

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer16.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer16.java
@@ -1,5 +1,28 @@
 package aula17.labs;
 
 public class Exer16 {
+	/*
+	 * 15. A série de Fibonacci é formada pela seqüência
+	 * 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o
+	 * n−ésimo termo.
+	 */
+	//Loiane resolução
+
+	public static void main(String[] args) {
+		int primeiro = 1;
+		int segundo =  1;
+		int proximo = 0;
+		
+		System.out.println(primeiro);
+		System.out.println(segundo);
+		
+		while(proximo <= 500){
+			proximo = primeiro + segundo;
+			primeiro = segundo;
+			segundo = proximo;
+			
+			System.out.println(proximo);
+		}
+	}
 
 }
