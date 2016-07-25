ae10faf9720e947986b978890fc6df6aab980c37
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 10:05:53 -0300

    exercicio 31 - calculo percentual de salario (simples)

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer31.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer31.java
@@ -1,5 +1,8 @@
 package aula17.labs;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exer31 {
 	/*
 	 * 31. Um funcionário de uma empresa recebe aumento salarial anualmente:
@@ -13,4 +16,24 @@ public class Exer31 {
 	 * isto, altere o programa permitindo que o usuário digite o salário inicial
 	 * do funcionário.
 	 */
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		double salario = 1000;
+		
+		double percentual = 1.5;
+		
+		salario += (salario/100)*percentual;
+		
+		DecimalFormat formato = new DecimalFormat("###,###.##");
+		for(int i= 1997; i <= 2015 ; i++ ){
+			percentual *= 2;
+			salario += (salario/100)*percentual;
+			
+			System.out.println( i + " = " + formato.format(salario) + " - " + percentual + " % ");
+			
+		}
+	}
+	
 }
