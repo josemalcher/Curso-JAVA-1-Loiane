39f740ca64949f04698311c3f51adf4b55ab8328
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 13:33:34 -0300

    exercicio 34 - SOma os termos de uma sequencia 1/n

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer34.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer34.java
@@ -1,8 +1,25 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer34 {
 	/*
 	 * 34. Sendo H= 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que
 	 * calcule o valor de H com N termos.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite o valor de n ");
+		int n = leitor.nextInt();
+		
+		double soma = 0;
+		
+		for(int i = 1; i <= n; i++){
+			soma += 1/i;
+		}
+		
+		System.out.println("Soma " + soma);
+		
+	}
 }
