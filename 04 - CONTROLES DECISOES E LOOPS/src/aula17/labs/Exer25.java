d162ed4754971fb2df82f1e8b95cf298d3eb3f59
José Malcher Jr <malcher.malch@gmail.com>
2016-07-24 23:33:06 -0300

    exercicio 25 - caixa registradora recebe produtos, calcula troco e totais

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer25.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer25.java
@@ -1,5 +1,7 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer25 {
 	/*
 	 * 25. O Sr. Manoel Joaquim expandiu seus negócios para além dos negócios de
@@ -20,6 +22,60 @@ o Total: R$ 9.00
 o Dinheiro: R$ 20.00
 o Troco: R$ 11.00
 o ...	 
-	 
+	 Loiane
 	 */
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		boolean sair = false;
+		String continuarCompra;
+		int quantidadeProdutos;
+		double preco;
+		double total;
+		String output;
+		double valorPago, troco;
+		
+		do{
+			System.out.println("Deseja informa uma nova compra? s = sim ou n = não ");
+			continuarCompra = leitor.next();
+			
+			if(continuarCompra.equalsIgnoreCase("s")){
+				output = "LOJAS TABAJARA \n";
+				System.out.println("Digite a quantidade de produtos comprados");
+				quantidadeProdutos = leitor.nextInt();
+				
+				total = 0;
+				
+				for(int i = 1; i <= quantidadeProdutos ; i++){
+					System.out.println("Informe o preço do produto " + i );
+					preco = leitor.nextDouble();
+					total =+ preco;
+					output += "Produto " + i + " R$ " + preco + "\n";
+					
+				}
+				output += "Total R$ " + total;
+				
+				System.out.println("Entre com o valor pago R$: ");
+				valorPago = leitor.nextDouble();
+				
+				output += " Dinheiro R$ " + valorPago + "\n";
+				
+				troco = total - valorPago;
+				
+				output += "Troco R$ " + troco ; 
+				
+				System.out.println(output);
+				
+			}else{
+				sair = true;
+				
+			}
+			
+			
+			
+		}while (!sair);
+		
+		
+	}
 }
