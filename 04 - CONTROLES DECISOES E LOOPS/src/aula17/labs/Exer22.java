23e0755f1c45cea1caefe4887b5e914dcbfd480c
José Malcher Jr <malcher.malch@gmail.com>
2016-07-24 21:56:39 -0300

    exercicio 22 - calculo da média de cd's

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer22.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer22.java
@@ -1,5 +1,7 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer22 {
 	/*
 	 * 22. Faça um programa que calcule o valor total investido por um
@@ -7,4 +9,22 @@ public class Exer22 {
 	 * deles. O usuário deverá informar a quantidade de CDs e o valor para em
 	 * cada um.
 	 */
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Qual a quantidade de CD's "); 
+		int cds = leitor.nextInt();
+		 
+		 
+		 double preco;
+		 double soma = 0 ;
+		 
+		 for(int i=1; i<=cds ; i++){
+			 System.out.println("Informa o valor do CD R$ "+i);
+			 preco = leitor.nextDouble();
+			 soma += preco;	
+		 }
+		 double media = soma/cds;
+		 System.out.println("A média gasta com cada cd é:"+media);
+	}
 }
