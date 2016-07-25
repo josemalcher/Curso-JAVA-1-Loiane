f43f16ee4d000d7ae3f57c204618311989eef995
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 00:32:25 -0300

    exercicio 28 - Verificar se o numeor é primo

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer28.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer28.java
@@ -1,5 +1,7 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer28 {
 	/*
 	 * 28. Os números primos possuem várias aplicações dentro da Computação, por
@@ -7,4 +9,23 @@ public class Exer28 {
 	 * por um e por ele mesmo. Faça um programa que peça um número inteiro e
 	 * determine se ele é ou não um número primo.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Entre com o numero para verificar");
+		int numero = leitor.nextInt();
+		
+		boolean primo = true;
+		
+		for( int i = 2 ; i < numero; i++){
+			if(numero % i == 0){
+				System.out.println("Não é primo - divisil por " + i);
+				primo = false;
+			}
+		}
+		if(primo){
+			System.out.println("é numeor primo");
+		}
+		
+	}
 }
