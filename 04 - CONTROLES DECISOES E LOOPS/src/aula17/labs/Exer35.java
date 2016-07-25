55359952abcf877250d53634a0c53b3d325e89be
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 13:38:18 -0300

    exercicio 35 e 36 - SOma os termos, calcula o valor de h com o valor de n termos + AJUSTES

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer35.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer35.java
@@ -1,8 +1,26 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer35 {
 	/*
 	 * 35. Faça um programa que mostre os n termos da Série a seguir: o S = 1/1
 	 * + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m. Imprima no final a soma da série.
 	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite o valor de n");
+		int n = scan.nextInt();
+		double soma = 0;
+
+		for (int i = 1, j = 1; i <= n; i++, j += 2) {
+
+			System.out.print(i + "/" + j + " + ");
+
+			soma += i / j;
+		}
+
+		System.out.println("\n Soma = " + soma);
+	}
 }
--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer36.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer36.java
@@ -8,19 +8,19 @@ public class Exer36 {
 	 * calcule o valor de H com N termos.
 	 */
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);
-		
-		System.out.println("Digire o valor de n");
-		
-		int n = leitor.nextInt();
-		
-		double soma = 0; 
-		
-		for(int i=1, j = 1; i<=n ; i++, j+=2){
-			System.out.print( i + "/"+ j + " + ");
-			soma += i/j;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite o valor de n:");
+		int n = scan.nextInt();
+
+		double soma = 0;
+
+		for (int i = 1; i <= n; i++) {
+
+			soma += 1 / i;
 		}
-		System.out.println("\n SOMA "+ soma);
+
+		System.out.println("Soma = " + soma);
 	}
-	
+
 }
