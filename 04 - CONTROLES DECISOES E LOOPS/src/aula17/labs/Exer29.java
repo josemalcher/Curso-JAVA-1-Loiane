6c1251b2e8c8f58dd685dbfc332bbb5a9690c706
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 00:41:08 -0300

    exercicio 29 - pega todos os numeros primos dentro de um intevalo definido de 1 até uma variavel

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer29.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer29.java
@@ -1,9 +1,36 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer29 {
 	/*
 	 * 29. Encontrar números primos é uma tarefa difícil. Faça um programa que
 	 * gera uma lista dos números primos existentes entre 1 e um número inteiro
 	 * informado pelo usuário.
+	 * 
+	 * Resolução Loiane
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Entre com um numero");
+		int numero = leitor.nextInt();
+		
+		boolean primo;
+		
+		for( int i = 1 ; i <= numero ; i++){
+			primo = true;
+			
+			for(int j = 2 ; j < i ; j++){
+				if( i % j == 0){
+					//System.out.println("Não é primo - divisível por "+ i);
+					primo = false;
+				}
+			}
+			if(primo){
+				System.out.println(i);
+			}
+		}
+	}
+	
 }
