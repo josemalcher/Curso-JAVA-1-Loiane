989fabfec2baf551a4b6fc31ea02910616c5fda8
José Malcher Jr <malcher.malch@gmail.com>
2016-07-25 11:46:51 -0300

    exercicio 32 - Cardapio com codigos, entrada do codigo e a quantidade do ítem, verificando a quantidade a pagar

--- 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer32.java
+++ 04 - CONTROLES DECISOES E LOOPS/src/aula17/labs/Exer32.java
@@ -1,5 +1,7 @@
 package aula17.labs;
 
+import java.util.Scanner;
+
 public class Exer32 {
 /*
  * 32. O cardápio de uma lanchonete é o seguinte:
@@ -17,8 +19,108 @@ item (preço * quantidade) e o total geral do pedido. Considere
 que o cliente deve informar quando o pedido deve ser
 encerrado.
  * 
+ * Resolução Loiane
  */
 	
-	
+	public static void main(String[] args) {
+		/*Scanner leitor = new Scanner(System.in);
+		
+		boolean naoTerminar = true;
+		int codigo, quantidade;
+		double total = 0;
+		String output = "";
+		
+		do{
+			System.out.println("Entre com o codigo e a quantidade, Digite 0 0  para siar");
+			codigo = leitor.nextInt();
+			quantidade = leitor.nextInt();
+			if(codigo == 0 && quantidade == 0){
+				naoTerminar = false;
+				output += "Total a pagar = R$ "+ total ; 
+			}else{
+				if (codigo == 100){
+                    output += "Cachorro Quente -> 1,20 * " + quantidade ;
+                    output += " = " + (1.2*quantidade) + "\n";
+                    total += 1.2*quantidade;
+                } else if (codigo == 101){
+                    output += "Bauru Simples -> 1,30 * " + quantidade ;
+                    output += " = " + (1.3*quantidade) + "\n";
+                    total += 1.3*quantidade;
+                } else if (codigo == 102){
+                    output += "Bauru com ovo -> 1,50 * " + quantidade ;
+                    output += " = " + (1.5*quantidade) + "\n";
+                    total += 1.5*quantidade;
+                } else if (codigo == 103){
+                    output += "Hambúrguer -> 1,20 * " + quantidade ;
+                    output += " = " + (1.2*quantidade) + "\n";
+                    total += 1.2*quantidade;
+                } else if (codigo == 104){
+                    output += "Cheeseburguer -> 1,30 * " + quantidade ;
+                    output += " = " + (1.3*quantidade) + "\n";
+                    total += 1.3*quantidade;
+                } else if (codigo == 105){
+                    output += "Refrigerante -> 1,00 * " + quantidade ;
+                    output += " = " + (1*quantidade) + "\n";
+                    total += 1*quantidade;
+}
+			}
+			
+			
+		}while(naoTerminar);
+		
+		System.out.println(output);
+		*/
+		
+Scanner scan = new Scanner(System.in);
+        
+        boolean naoTerminar = true;
+        int cod, qtd;
+        double total = 0;
+        String output = "";
+        
+        do {
+            
+            System.out.println("Digite o código e a quantidade. Digite 0 0 para sair.");
+            cod = scan.nextInt();
+            qtd = scan.nextInt();
+            
+            if (cod == 0 && qtd == 0){
+                naoTerminar = false;
+                output += "Total a pagar = " + total;
+            } else {
+                
+                if (cod == 100){
+                    output += "Cachorro Quente -> 1,20 * " + qtd ;
+                    output += " = " + (1.2*qtd) + "\n";
+                    total += 1.2*qtd;
+                } else if (cod == 101){
+                    output += "Bauru Simples -> 1,30 * " + qtd ;
+                    output += " = " + (1.3*qtd) + "\n";
+                    total += 1.3*qtd;
+                } else if (cod == 102){
+                    output += "Bauru com ovo -> 1,50 * " + qtd ;
+                    output += " = " + (1.5*qtd) + "\n";
+                    total += 1.5*qtd;
+                } else if (cod == 103){
+                    output += "Hambúrguer -> 1,20 * " + qtd ;
+                    output += " = " + (1.2*qtd) + "\n";
+                    total += 1.2*qtd;
+                } else if (cod == 104){
+                    output += "Cheeseburguer -> 1,30 * " + qtd ;
+                    output += " = " + (1.3*qtd) + "\n";
+                    total += 1.3*qtd;
+                } else if (cod == 105){
+                    output += "Refrigerante -> 1,00 * " + qtd ;
+                    output += " = " + (1*qtd) + "\n";
+                    total += 1*qtd;
+                }
+            }
+            
+        }while(naoTerminar);
+        
+System.out.println(output);
+		
+		
+	}
 	
 }
