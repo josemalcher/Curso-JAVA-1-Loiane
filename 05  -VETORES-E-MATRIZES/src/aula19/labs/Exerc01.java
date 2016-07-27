e5d9f95d355f2841d19edc00534de165794b5ed2
José Malcher Jr <malcher.malch@gmail.com>
2016-07-27 18:18:47 -0300

    Exerc 01 - vetor simples com 5 elemetos A -> para -> B

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc01.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc01.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc01 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,29 @@ public class Exerc01 {
 		 * mesmo tipo e tamanho e com os "mesmos" elementos do vetor A, ou seja,
 		 * B[i] = A[i].
 		 */
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[5];
+		int[] vetorB = new int[vetorA.length];
+		
+		for(int i = 0; i < vetorA.length ; i++){
+			System.out.println("Entre com o valor da posição "+ i);
+			vetorA[i] = leitor.nextInt();
+			
+			vetorB[i] = vetorA[i];
+		}
+		
+		System.out.print("Vetor A = ");
+		for(int i = 0 ; i < vetorA.length; i++){
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+		System.out.print("Vetor B = ");
+		for(int i = 0 ; i < vetorB.length; i++){
+			System.out.print(vetorB[i] + " ");
+		}
+		System.out.println();
 
 	}
 
