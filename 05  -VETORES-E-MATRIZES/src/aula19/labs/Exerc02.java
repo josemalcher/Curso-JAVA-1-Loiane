5ac647d09fe2d2f6a8fe0357d32528455fd6e3cf
José Malcher Jr <malcher.malch@gmail.com>
2016-07-27 18:44:42 -0300

    Exerc 02 - vetor simples com 8 elemetos VetorA -> VetorB * 2

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc02.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc02.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc02 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,30 @@ public class Exerc02 {
 		 * mesmo tipo e tamanho e com os elementos do vetor A multiplicados por
 		 * 2, ou seja: B[i] = A[i] * 2.
 		 */
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] VetorA = new int[8];
+		int[] VetorB = new int[VetorA.length];
+		
+		for(int i = 0 ; i < VetorA.length ; i++){
+			System.out.println("Digite 8 inteiros");
+			VetorA[i] = leitor.nextInt();
+			
+			VetorB[i] = VetorA[i] * 2;
+		}
+		System.out.print("Vetor A -> ");
+		for(int vetor : VetorA){
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		
+		System.out.print("Vetor B -> ");
+		for(int vetor : VetorB){
+			System.out.print(vetor + " ");
+		}
+		
+		
 
 	}
 
