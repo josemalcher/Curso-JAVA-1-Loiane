bc6a65365c39155fde3df1fe41260fbacaa87db6
José Malcher Jr <malcher.malch@gmail.com>
2016-07-30 18:32:23 -0300

    Exerc 05 - Exercicio simples, semelhante ao anterior, A -> B = B * A * i

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc05.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc05.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc05 {
 	/*
 	 * 5. Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
@@ -7,4 +9,29 @@ public class Exerc05 {
 	 * respectivo elemento de A multiplicado por sua posição (ou índice), ou
 	 * seja: B[i] = A[i] * i.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor  = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		
+		for(int i = 1; i < vetorA.length; i++){
+			System.out.println("Digite o " + i);
+			vetorA[i] = leitor.nextInt();
+			
+			vetorB[i] = vetorA[i]*i;
+					
+		}
+		
+		System.out.println();
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		
+		System.out.println();
+		
+		for(int vetor : vetorB){
+			System.out.print(vetor + " ");
+		}
+	}
 }
