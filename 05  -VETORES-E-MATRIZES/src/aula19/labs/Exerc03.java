4c26e4923522edd3f303f24de33fd4ffcc9c07b3
José Malcher Jr <malcher.malch@gmail.com>
2016-07-30 02:10:48 -0300

    Exerc 03 - Vetor com 15 elmentos A->B e B = A * A

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc03.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc03.java
@@ -1,5 +1,8 @@
 package aula19.labs;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Exerc03 {
 
 	public static void main(String[] args) {
@@ -9,6 +12,30 @@ public class Exerc03 {
 		 * ser o quadrado do respectivo elemento de A, ou seja: B[i] = A[i] *
 		 * A[I].
 		 */
+		
+		Scanner leitor  = new Scanner(System.in);
+		
+		int[] vetorA = new int[15];
+		double[] vetorB = new double[vetorA.length];
+		
+		for(int i = 0; i < vetorA.length ; i++){
+			System.out.println("Digite 15 inteitos "+ i);
+			vetorA[i] = leitor.nextInt();
+			
+			vetorB[i] = Math.sqrt(vetorA[i]);
+		}
+		System.out.print("Vetor A ->");
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		
+		DecimalFormat formatador = new DecimalFormat("###,###.##");
+		
+		System.out.print("Vetor B ->");
+		for(double vetor : vetorB){
+			System.out.print(formatador.format(vetor) + " ");
+		}
 
 	}
 
