3a8d7958f2e945224160b1984cb8858fe80eba7c
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 20:01:07 -0300

    Exerc 13 - vetores -  A |  SOMA = soma dos numeros multimos de 5

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc13.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc13.java
@@ -1,9 +1,38 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc13 {
 	/*
 	 * 13. Criar um vetor A com 10 elementos inteiros. Implementar um programa
 	 * que determine a soma dos elementos armazenados neste vetor que são
 	 * múltiplos de 5.
 	 */
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite Vetor A - "+i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		int soma = 0;
+		for(int i = 0 ; i < vetorA.length ; i++){
+			if(vetorA[i] % 5 == 0){
+				soma += vetorA[i];
+			}
+		}
+		
+		
+		System.out.print("Vetor A = ");
+		for(int vetor : vetorA){
+			System.out.println(vetor + " ");
+		}
+		
+		System.out.println("A soma dos multiplos de 5 é "+soma);
+		
+	}
 }
