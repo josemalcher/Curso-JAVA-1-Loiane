50ddde40d6592ca013bdfcb41cb1bf2d64149469
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 13:00:58 -0300

    Exerc 08 - vetores - exercicios simples A B | c = A * B

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc08.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc08.java
@@ -1,9 +1,54 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc08 {
 	/*
 	 * 8. Criar dois vetores A e B cada um com 10 elementos inteiros. Construir
 	 * um vetor C, onde cada elemento de C é a multiplicação dos respectivos
 	 * elementos em A e B, ou seja: C[i] = A[i] * B[i].
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		int[] vetorC = new int[vetorA.length];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite os valores de A: "+ i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0 ; i < vetorB.length ; i++){
+			System.out.println("Digite o valores de B: "+ i);
+			vetorB[i] = leitor.nextInt();
+		}
+		
+		for(int i = 0 ; i < vetorC.length ; i++){
+			vetorC[i] = vetorA[i] * vetorB[i];
+		}
+		
+		System.out.println();
+		
+		System.out.println("Resultado = ");
+		
+		System.out.print("Vetor A = " );
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		System.out.print("Vetor B = " );
+		for(int vetor : vetorB){
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		System.out.print("Vetor C = " );
+		for(int vetor : vetorC){
+			System.out.print(vetor + " ");
+		}
+		
+	}
 }
