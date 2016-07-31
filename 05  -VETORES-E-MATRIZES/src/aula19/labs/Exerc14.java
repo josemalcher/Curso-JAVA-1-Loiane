77d36465afc2078cc85f2ec33aa3951f587b1b49
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 20:26:07 -0300

    Exerc 14 - vetores -  A - soma e média dos numeros impares do vetor|

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc14.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc14.java
@@ -1,9 +1,46 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc14 {
 	/*
 	 * 14. Criar um vetor A com 10 elementos inteiros. Implementar um programa
 	 * que defina e escreva a média aritmética simples dos elementos ímpares
 	 * armazenados neste vetor.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite Vetor A - "+i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+
+		int soma = 0;
+		int impar = 0;
+		double media = 0;
+		for(int i = 0 ; i < vetorA.length ; i++){
+			if(vetorA[i] % 2 != 0){
+				soma =+ vetorA[i];
+				impar++;
+			}
+		}
+		media = soma/impar;
+		
+		System.out.print("Vetor A = ");
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Soma = "+ soma);
+		System.out.println("Numeros de impares = "+impar);
+		System.out.println("Média = "+media);
+		
+			
+	}
 }
