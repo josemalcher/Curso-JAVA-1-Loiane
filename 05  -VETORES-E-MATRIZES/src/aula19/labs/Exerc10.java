d3c4ef7f17beda80a59e26143dd1960f835551fb
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 18:17:49 -0300

    Exerc 10 - vetores - exercicios simples A B |  B = A % 2

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc10.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc10.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc10 {
 	/*
 	 * 10. Criar um vetor A com 10 elementos inteiros. Construir um vetor B de
@@ -7,4 +9,33 @@ public class Exerc10 {
 	 * resto da divisão do respectivo elemento de A por 2 (dois), ou seja: B[i]
 	 * := A[i] % 2.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite Vetor A "+i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			vetorB[i] = vetorA[i] % 2;
+		}
+		
+		System.out.println("**************");
+		System.out.println("RESULTADO: ");
+		
+		System.out.print("Vetor A = ");
+		for (int vetor : vetorA) {
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		System.out.print("Vetor B = ");
+		for (int vetor : vetorB) {
+			System.out.print(vetor + " ");
+		}
+		
+	}
 }
