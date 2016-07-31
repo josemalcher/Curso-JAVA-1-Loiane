c0da99f84d0e96ae5489e1d50db21262dbfeff23
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 19:42:45 -0300

    Exerc 12 - vetores -  A |  SOMA = soma dos numeros

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc12.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc12.java
@@ -1,9 +1,35 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc12 {
 	/*
 	 * 12. Criar um vetor A com 10 elementos inteiros. Implementar um programa
 	 * que defina e escreva a soma de todos os elementos armazenados neste
 	 * vetor.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite Vetor A - "+ i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		int soma = 0;
+		
+		for(int i =0 ; i < vetorA.length ; i++){
+			soma += vetorA[i];
+		}
+		System.out.println();
+		System.out.print("Vetor A = ");
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		
+		System.out.println("SOma de todos os números: "+soma);
+		
+	}
 }
