c77e8495a4d2f449f9b5a096019a994b128d976e
José Malcher Jr <malcher.malch@gmail.com>
2016-07-31 18:55:29 -0300

    Exerc 11 - vetores - exercicios simples A |  A - total numeros pares

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc11.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc11.java
@@ -1,9 +1,38 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc11 {
 	/*
 	 * 11. Criar um vetor A com 10 elementos inteiros. Implementar um programa
 	 * que defina e escreva a quantidade de elementos armazenados neste vetor
 	 * que são pares.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length; i++){
+			System.out.println("digite vetor A -> "+ i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		System.out.print("Vetor A = ");
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.print(vetorA[i] + " ");
+		}
+		
+		int totalPares =0;
+		for(int i = 0 ; i < vetorA.length ; i++){
+			if(vetorA[i] % 2 == 0){
+				totalPares++;
+			}
+		}
+		System.out.println();
+		System.out.println("*****************");
+		System.out.println("Total de Pares = " + totalPares);
+		
+		
+	}
 }
