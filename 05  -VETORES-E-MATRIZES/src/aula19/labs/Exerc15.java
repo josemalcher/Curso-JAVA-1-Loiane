e7bf0dd962b54148f10be70e741e1a849c987e06
José Malcher Jr <malcher.malch@gmail.com>
2016-08-02 10:16:34 -0300

    Exerc 15 - vetor A[10] calcular a poncentagem de pares e impares

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc15.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc15.java
@@ -1,9 +1,51 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc15 {
 /*
  * 15. Criar um vetor A com 10 elementos inteiros. Desenvolver um programa
 que defina o percentual de elementos pares e ímpares,
 respectivamente, armazenados neste vetor.
+RESOLUÇÃO Loiane
  */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite vetor A - "+ i);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		int totalImpar = 0;
+		for(int i = 0 ; i < vetorA.length ; i++){
+			if(vetorA[i] % 2 != 0){
+				totalImpar++;
+			}
+		}
+		
+		int totalPar = vetorA.length - totalImpar;
+		
+		/*
+		 * Explicação
+		 * vetorA.lenght - 100%
+		 * par			 -   X
+		 * X * vetorA.lenght = (par * 100) / vetorA.lenght
+		 *  X == (par * 100)/vetorA.lenght
+		 * 
+		 */
+		double porcenPar = (totalPar * 100)/vetorA.length;
+		double porcenImpar = 100 - porcenPar;
+		
+		System.out.print("Vetor A = ");
+		for(int vetor : vetorA){
+			System.out.print(vetor + " ");
+		}
+		System.out.println();
+		System.out.println("Porcentagem de Pares = "+porcenPar);
+		System.out.println("Porcentagem de IMpares = "+porcenImpar);
+		
+	}
 }
