9d9a978ab9398aca2ad841aca8cf69c6b6ef7032
José Malcher Jr <malcher.malch@gmail.com>
2016-08-03 14:17:40 -0300

    Exerc 21 - Vetor aleatório 1 o 0, calcula a quantidade de 0 e 1 e suas porcentagem

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc21.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc21.java
@@ -9,6 +9,41 @@ public class Exerc21 {
 	 * iguais a 0 e 1, sugestão: A[i] = (int)Math.round(Math.random() * 1);
 	 * Pede-se para implementar um programa que determine o percentual de
 	 * números 0's e 1's existentes no vetor A.
+	 * 
+	 * resolução: http://www.pb.utfpr.edu.br/omero/Java/Fontes/Lista%20Vetores/_21.Htm
+	 * 
 	 */
-
+	public static void main(String[] args) {
+		
+		int n = 10;
+		int a[] = new int[n];
+		int i;
+		int ctum = 0;
+		int ctzero = 0;
+		float pum, pzero;
+		
+		for(i = 0 ; i < n ; i++){
+			a[i] = (int)Math.round(Math.random()*1);
+		}
+		
+		for(i = 0 ; i < n ; i++){
+			System.out.printf("a[%d] = %d\n",i , a[i]);
+			
+			if(a[i] == 0){
+				ctzero = ctzero + 1;
+			}else{
+				ctum = ctum + 1;
+			}
+		}
+		
+		pzero = (ctzero / (float)n) * 100;
+		pum   = (ctum / (float)n) * 100;
+		
+		System.out.printf("\n Existem %d números O's, ou seja, %.2f%%", ctzero, pzero);
+	    System.out.printf("\n Existem %d números 1's, ou seja, %.2f%%\n", ctum, pum);
+		
+		
+		
+		
+	}
 }
