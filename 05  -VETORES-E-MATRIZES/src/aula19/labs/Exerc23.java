73f98e86205a1ba1a64a6b1401cc045b346b50a4
José Malcher Jr <malcher.malch@gmail.com>
2016-08-03 15:36:29 -0300

    Exerc 23 - Vetor A com pares se houver entrada de algum impar programa finaliza

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc23.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc23.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc23 {
 	/*
 	 * 23. Criar um vetor A com 10 elementos inteiros. Desenvolver um programa
@@ -8,4 +10,47 @@ public class Exerc23 {
 	 * os elementos do vetor deve ser encerrado, como sugestão: utilize uma
 	 * variável do tipo flag para atingir este propósito.
 	 */
+	public static void main(String[] args) {
+/*		
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		boolean todosPares;
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.printf("digite o %2do. elemento de %d: ", (i+1),vetorA.length);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		todosPares = true;
+		for( int i = 0 ; i < vetorA.length && (todosPares ==true) ; i++){
+			if( (vetorA[i] % 2) != 0){
+				todosPares = false; // encontrou um impar
+			}
+		}
+		if(todosPares == true){
+			System.out.println("TOdoso os elementos são pares");
+		}else{
+			System.out.println("Nem todos são Pares");
+		}
+		
+	*/	
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.println("Digite os numeros "+ i );
+			vetorA[i] = leitor.nextInt();
+			
+			if(vetorA[i] % 2 != 0 ){
+				System.out.println("Entrou impar, FIM do sistema");
+				break;
+				
+			}
+		}
+		
+		
+	}
 }
