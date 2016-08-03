c61f9c6b94edf9712da9d499f80f026502c75978
José Malcher Jr <malcher.malch@gmail.com>
2016-08-03 16:28:36 -0300

    Exerc 24 - vetor de verificação se é palindromo ou não - solução de Omero Francisco

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc24.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc24.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc24 {
 	/*
 	 * 24. Números palíndromos são aqueles que escritos da direita para a
@@ -10,4 +12,36 @@ public class Exerc24 {
 	 * elemento do vetor é igual ao penúltimo e assim por diante até verificar
 	 * todos os elementos ou chegar a conclusão que o vetor não é um palíndromo.
 	 */
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for(int i = 0 ; i < vetorA.length ; i++){
+			System.out.printf("Informe %2do. elemento %d: ",(i+1), vetorA.length);
+			vetorA[i] = leitor.nextInt();
+		}
+		
+		int i = 0 ; // posição iniciais do vetor
+		int j = (vetorA.length - 1); //posições finais do vetor
+		boolean ePalindromo = true; //flag eh palindromo ou não
+		
+		while ((i < j)&& (ePalindromo == true)){
+			if(vetorA[i] != vetorA[j]){
+				ePalindromo = false;
+			}else{
+				i++;
+				j--;
+			}
+		}
+		
+		if(ePalindromo == true){
+			System.out.println("Os elementos do vetor formam um palindromo");
+		}else{
+			System.out.println("Os elementos do vetor não formam um palindromo");
+		}
+		
+	}
+	
 }
