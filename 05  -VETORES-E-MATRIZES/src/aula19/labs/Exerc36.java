77a297ae634c900c0dab2c03d564809c7a3e94b6
José Malcher Jr <malcher.malch@gmail.com>
2016-08-04 17:07:43 -0300

    Exerc 36 - vetor com 11 elementos . Dados elevados ao expoente 2

--- 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc36.java
+++ 05  -VETORES-E-MATRIZES/src/aula19/labs/Exerc36.java
@@ -1,5 +1,7 @@
 package aula19.labs;
 
+import java.util.Scanner;
+
 public class Exerc36 {
 	/*
 	 * 36. Criar um vetor A com 11 elementos, indexados de 0 até 10. Sendo que
@@ -7,4 +9,32 @@ public class Exerc36 {
 	 * expoente igual a posição do respectivo elemento, ou seja: A[i] = 2i.
 	 * Sugestão int A[11];
 	 */
+	public static void main(String[] args) {
+		/*
+		Scanner scan = new Scanner(System.in);
+
+		double[] vetorA = new double[11];
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			vetorA[i] = Math.pow(2, i);
+		}
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();*/
+		
+		int n = 11;
+	      int i, pot = 1, a[] = new int[n];
+
+	      for (i=0; i<n; i++) {
+	        a[i] = pot;
+	        System.out.printf("2 elevado a %d = %d.\n", i, a[i]);
+
+	        pot = pot * 2;
+	      }
+		
+	}
 }
