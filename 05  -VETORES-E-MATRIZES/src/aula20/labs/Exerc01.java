1ce7ee0841c19651c7ba0e90bffdabe0e8d4ed99
José Malcher Jr <malcher.malch@gmail.com>
2016-08-09 12:39:58 -0300

    Exerc 01 - matriz com 4x4 com numeros aleatorios determinar qual numero é maior e menor, e qual sua localização linaXcoluna

--- 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc01.java
+++ 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc01.java
@@ -1,9 +1,46 @@
 package aula20.labs;
 
+import java.util.Random;
+
 public class Exerc01 {
 	/*
 	 * 1. Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0- 9.
 	 * Após isso determine o maior número da matriz e a sua posição (linha,
 	 * coluna).
 	 */
+	public static void main(String[] args) {
+		int[][] numerosAleatorios = new int[4][4];
+		
+		Random numeroRamdom = new Random();
+		
+		for(int i = 0 ; i < numerosAleatorios.length ; i++){
+			for(int j = 0 ; j < numerosAleatorios[i].length ; j++){
+				numerosAleatorios[i][j] = numeroRamdom.nextInt(100);
+			}
+		}
+		
+		int maior = 0;
+		int linha = 0; 
+		int colun = 0;
+		for(int i = 0 ; i < numerosAleatorios.length ; i++){
+			for(int j = 0 ; j < numerosAleatorios[i].length ; j++){
+				if(numerosAleatorios[i][j] > maior){
+					maior = numerosAleatorios[i][j];
+					linha = i;
+					colun = j;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < numerosAleatorios.length ; i++){
+			for (int j = 0 ; j < numerosAleatorios[i].length ; j++){
+				System.out.print(numerosAleatorios[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Maior valor = " + maior);
+		System.out.println("Linha = "+ linha);
+		System.out.println("Coluna = "+ colun);	
+	}
 }
