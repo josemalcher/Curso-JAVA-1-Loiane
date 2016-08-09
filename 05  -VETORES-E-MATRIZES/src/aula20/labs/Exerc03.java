0f14cf2062b94573cc5b4f308eda0fe0aa2864d6
José Malcher Jr <malcher.malch@gmail.com>
2016-08-09 16:04:06 -0300

    Exerc 03 - Matriz 3x3 . verifica a quantidade  de pares e impares

--- 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc03.java
+++ 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc03.java
@@ -1,9 +1,46 @@
 package aula20.labs;
 
+import java.util.Scanner;
+
 public class Exerc03 {
 	/*
 	 * 3. Capture do teclado valores para preenchimento de uma matriz M 3x3.
 	 * Após a captura imprima a matriz criada e encontre a quantidade de números
 	 * pares, a quantidade de números ímpares.
 	 */
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int[][] numeros = new int[3][3];
+		
+		for(int i = 0; i < numeros.length ; i++){
+			for(int j = 0 ; j < numeros[i].length ; j++){
+				System.out.println("Entre como valor da posição [" + i + "," + j + "]");
+				numeros[i][j] = leitor.nextInt(); 
+			}
+		}
+		
+		int quantidadePares 	= 0 ; 
+		int quantidadeImpares 	= 0 ;
+		
+		for(int i = 0 ; i < numeros.length ; i++){
+			for(int j = 0; j < numeros[i].length ; j++){
+				if(numeros[i][j] % 2 == 0){
+					quantidadePares++;
+				}else{
+					quantidadeImpares++;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i< numeros.length ; i++){
+			for(int j=0 ; j < numeros[i].length ; j++){
+				System.out.print(numeros[i][j] + " ");
+			}
+			System.out.println();
+		}
+			System.out.println("Pares = " + quantidadePares);
+			System.out.println("Impares = " + quantidadeImpares);
+		
+	}
 }
