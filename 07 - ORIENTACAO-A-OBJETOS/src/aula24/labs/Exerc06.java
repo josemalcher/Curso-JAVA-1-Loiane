a2c4ae2693b78d298ec20ccfeb82f036ab4c7dfa
José Malcher Jr <malcher.malch@gmail.com>
2016-08-11 15:41:21 -0300

    Exerc 06 + Classe Contato - classe simples de demonstração

--- 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc06.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc06.java
@@ -4,4 +4,17 @@ public class Exerc06 {
 	/*
 	 * 6. Crie uma classe que represente um contato da agenda do seu celular.
 	 */
+	public static void main(String[] args) {
+		Contato contato1 = new Contato();
+		contato1.nome = "jose";
+		contato1.endereco = "Av. qualquer";
+		contato1.email = "jose@email.com";
+
+		contato1.telefones = new String[5];
+		contato1.telefones[0] = "99999-9999";
+		contato1.telefones[1] = "99999-9998";
+		contato1.telefones[2] = "99999-9997";
+		
+		//impressões... etc..
+	}
 }
