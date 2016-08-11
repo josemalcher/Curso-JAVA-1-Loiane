591a97ead062533de53680703894c5220d8c27ce
José Malcher Jr <malcher.malch@gmail.com>
2016-08-11 14:52:12 -0300

    Exerc 02 + Classe Livro - Exercicio simples e criação do objeto e das classes.

--- 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc02.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc02.java
@@ -5,4 +5,16 @@ public class Exerc02 {
 	 * 2. Crie uma classe Livro que represente os dados básicos de um livro, sem
 	 * se preocupar com a sua finalidade.
 	 */
+	public static void main(String[] args) {
+		
+		Livro livroObjeto = new Livro();
+		
+		livroObjeto.nome = "Nome do livro";
+		livroObjeto.autor = "Autor desconhecido";
+		livroObjeto.anoLacamento = 1999;
+		
+		System.out.println("Nome " + livroObjeto.nome);
+		System.out.println("Ano Lançamento ; "+ livroObjeto.anoLacamento);
+		System.out.println("ISBN " +  livroObjeto.isbn);
+	}
 }
---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Livro.java
@@ -0,0 +1,13 @@
+package aula24.labs;
+
+public class Livro {
+	
+	String nome;
+	String autor;
+	String isbn;
+	
+	int quantidadePaginas;
+	int anoLacamento;
+	
+	
+}
