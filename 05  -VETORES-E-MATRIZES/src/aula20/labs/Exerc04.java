e680de4321af8a1c0386e1567a1d15bf95383fb5
José Malcher Jr <malcher.malch@gmail.com>
2016-08-10 21:05:59 -0300

    Exerc 04 - Agenda simples dia e hora e opçoes de adição e verificação - versão com erro!

--- 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc04.java
+++ 05  -VETORES-E-MATRIZES/src/aula20/labs/Exerc04.java
@@ -1,5 +1,7 @@
 package aula20.labs;
 
+import java.util.Scanner;
+
 public class Exerc04 {
 	/*
 	 * 4. Faça um programa para armazenar em uma matriz os compromissos de uma
@@ -9,5 +11,98 @@ public class Exerc04 {
 	 * que deseja alterar e a hora, entrando em seguida com o compromisso, ou
 	 * então, o usuário pode também consultar a agenda, fornecendo o dia e a
 	 * hora para obter o compromisso armazenado.
+	 * 
+	 * Resolução da Loiane
+	 * 
 	 */
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+
+		String[][] comprimissos = new String[31][24];
+
+		boolean sair = false;
+		byte opcao;
+
+		while (!sair) {
+			System.out.println("Digite 1 para adicionar comprimisso");
+			System.out.println("Digite 2 para verificar comprimissos");
+			System.out.println("digite 0 (zero) para sair");
+
+			opcao = leitor.nextByte();
+
+			if (opcao == 1) {
+
+				boolean diaValido = false;
+				int dia = 0;
+				while (!diaValido) {
+					System.out.println("Entre com o dia do mês");
+					dia = leitor.nextInt();
+
+					if (dia > 0 && dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Dia não válido, digite entre 0 e 31");
+					}
+				}
+
+				boolean horavalida = false;
+				int hora = 0;
+
+				while (!horavalida) {
+					System.out.println("DIgite a hora do comprimisso");
+					hora = leitor.nextInt();
+					if (hora >= 0 && hora <= 24) {
+						horavalida = true;
+					} else {
+						System.out.println("Hora inválida, digite novamente!!");
+					}
+				}
+
+				dia--; // para caber na numeração do arry é feito esse ajuste
+				System.out.println("Digite o compromisso");
+				comprimissos[dia][hora] = leitor.next();
+
+			} else if (opcao == 2) {
+
+				boolean diaValido = false;
+				int dia = 0;
+
+				while (!diaValido) {
+					System.out.println("Digite o DIA do mês: ");
+					dia = leitor.nextInt();
+
+					if (dia > 0 && dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Digite um DIA VALIDO!!, DIgite novamente");
+					}
+				}
+
+				boolean horaValida = false;
+				int hora = 0;
+
+				while (!horaValida) {
+					System.out.println("Entre com a hora do compromisso");
+					hora = leitor.nextInt();
+
+					if (hora >= 0 && hora <= 24) {
+						horaValida = true;
+					}
+
+				}
+
+				dia--;
+				System.out.println("O comprimisso agendado é: ");
+				System.out.println(comprimissos[dia][hora]);
+
+			} else if (opcao == 0) {
+				sair = true;
+			} else {
+				System.out.println("Opção inválida, digite novamente");
+			}
+
+		}
+
+	}
 }
