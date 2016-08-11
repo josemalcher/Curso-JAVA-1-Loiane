02f359906ef9ed61efb9c167574c3423db44d57e
José Malcher Jr <malcher.malch@gmail.com>
2016-08-11 15:37:15 -0300

    Exerc 05 + Classe ContaCorrente - Exercicio simples de classes e seu uso.

--- 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc05.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc05.java
@@ -6,4 +6,14 @@ public class Exerc05 {
 	 * número, um saldo, um status que informa se ela é especial ou não, um
 	 * limite.
 	 */
+	public static void main(String[] args) {
+		ContaCorrente conta = new ContaCorrente();
+		conta.numero = "123";
+		conta.agencia = "4321";
+		conta.especial = true;
+		conta.limiteEspecial = 1000;
+		conta.saldo = 310;
+
+		System.out.println("Saldo da conta " + conta.numero + " = " + conta.saldo);
+	}
 }
