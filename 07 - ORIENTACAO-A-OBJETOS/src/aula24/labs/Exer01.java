5b309ce0b0f129c0ce220311fc43cbd4c8751e4e
José Malcher Jr <malcher.malch@gmail.com>
2016-08-11 14:10:34 -0300

    Exerc 01 + classe Lampada - Exercicio simples para declaração e atribuição de variáveis

--- 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exer01.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exer01.java
@@ -5,4 +5,20 @@ public class Exer01 {
 	 * 1. Escreva uma classe para representar uma lâmpada que está à venda em um
 	 * supermercado.
 	 */
+	
+	public static void main(String[] args) {
+		
+		Lampada lampTipo1 = new Lampada();
+		
+		lampTipo1.modelo = "A01";
+		lampTipo1.tensao = "Bivolt";
+		lampTipo1.cor = "Branca";
+		//etc...
+		
+		//Atenção especial aqui:
+		lampTipo1.tipos = new String[5];
+		lampTipo1.tipos[0] = "Para Abajur";
+		lampTipo1.tipos[1] = "Carro";
+	}
+	
 }
---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Lampada.java
@@ -0,0 +1,16 @@
+package aula24.labs;
+
+public class Lampada {
+
+	String modelo;
+	String tensao;
+	String cor;
+	String tipoLuz;
+	int potencia;
+	int garantiaMeses;
+	
+	boolean tipoAbajur;
+	
+	String[] tipos;
+
+}
