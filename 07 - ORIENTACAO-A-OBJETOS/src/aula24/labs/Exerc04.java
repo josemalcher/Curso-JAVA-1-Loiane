36e27a1b9294f0a1ff8c52806df0974d751b23d8
José Malcher Jr <malcher.malch@gmail.com>
2016-08-11 15:33:41 -0300

    Exerc 04 + Classe LivroBiblioteca - classe simple e arquivo de teste da classe.

--- 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc04.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/Exerc04.java
@@ -1,9 +1,27 @@
 package aula24.labs;
 
+import java.util.Date;
+
 public class Exerc04 {
 	/*
 	 * 4. Usando o resultado do modelo “Livro” como base, crie uma classe
 	 * “LivroDeBiblioteca” que represente os dados básicos de um livro de uma
 	 * biblioteca, que pode ser emprestado a leitores.
 	 */
+	public static void main(String[] args) {
+		LivroBiblioteca livroEmprestimo = new LivroBiblioteca();
+		
+		livroEmprestimo.nome = "Livro Qualquer";
+		livroEmprestimo.autor = "Autor qualquer";
+		
+		livroEmprestimo.empretado = true;
+		
+		livroEmprestimo.dataEntrega = new Date();
+		livroEmprestimo.nomeUsuario = "Jose";
+		
+		System.out.println("Nome " + livroEmprestimo.nome);
+		//etc...etc...
+		
+		
+	}
 }
---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula24/labs/LivroBiblioteca.java
@@ -0,0 +1,16 @@
+package aula24.labs;
+
+import java.util.Date;
+
+public class LivroBiblioteca {
+	String nome;
+	String autor;
+	String isbn;
+
+	int quantidadePaginas;
+	int anoLacamento;
+	
+	boolean empretado;
+	Date dataEntrega;
+	String nomeUsuario;
+}
