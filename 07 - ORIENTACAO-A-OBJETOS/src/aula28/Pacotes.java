6e6d635db297459460e3ce3f6132c0db814c086f
José Malcher Jr <malcher.malch@gmail.com>
2016-08-12 16:19:53 -0300

    Aula 28 - Pacotes e Impors em JAVA. Alguns exemplos

---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula28/Pacotes.java
@@ -0,0 +1,14 @@
+package aula28;
+
+import aula26.Carro;
+import aula27.labs.Exerc01;
+
+public class Pacotes {
+	public static void main(String[] args) {
+		Carro carro;
+		aula24.Carro carro2;
+		
+		Exerc01 exercicio10;
+		
+	}
+}
