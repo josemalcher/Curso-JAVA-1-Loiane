ee4ba9e5cd1b65b89fb99c9aa2e47bf6b48ea46b
José Malcher Jr <malcher.malch@gmail.com>
2016-08-15 01:01:59 -0300

    Aula 32 - Encapsulamento: metodos get e set

---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula32/Carro.java
@@ -0,0 +1,42 @@
+package aula32;
+
+public class Carro {
+	private String marca;
+	private String modelo;
+	private int numeroPassageiros;
+	private double capCombustivel;
+	private double consumoCombustivel;
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getNumeroPassageiros() {
+		return numeroPassageiros;
+	}
+	public void setNumeroPassageiros(int numeroPassageiros) {
+		this.numeroPassageiros = numeroPassageiros;
+	}
+	public double getCapCombustivel() {
+		return capCombustivel;
+	}
+	public void setCapCombustivel(double capCombustivel) {
+		this.capCombustivel = capCombustivel;
+	}
+	public double getConsumoCombustivel() {
+		return consumoCombustivel;
+	}
+	public void setConsumoCombustivel(double consumoCombustivel) {
+		this.consumoCombustivel = consumoCombustivel;
+	}
+	
+	
+	
+}
---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula32/TesteCarro.java
@@ -0,0 +1,14 @@
+package aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro carro01 = new Carro();
+		
+		carro01.setMarca("Santana");
+		System.out.println(carro01.getMarca());
+
+	}
+
+}
