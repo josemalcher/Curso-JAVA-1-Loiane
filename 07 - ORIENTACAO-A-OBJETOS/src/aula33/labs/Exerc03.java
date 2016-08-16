3a3acbb6b6fa74f6f6472cd126fc80fd6b073f1e
José Malcher Jr <malcher.malch@gmail.com>
2016-08-16 16:57:10 -0300

    Exerc 03 - Classe Aluno com metodos e arrays para guardar as notas

--- 07 - ORIENTACAO-A-OBJETOS/src/aula33/labs/Exerc03.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula33/labs/Exerc03.java
@@ -1,5 +1,7 @@
 package aula33.labs;
 
+import java.util.Scanner;
+
 public class Exerc03 {
 	/*
 	 * http://www.loiane.com/2013/10/curso-java-basico-java-se-gratuito/
@@ -19,5 +21,48 @@ public class Exerc03 {
 	 * usuário e ao final informa o nome das disciplinas, mostra as notas e
 	 * mostra se o aluno foi aprovado ou não.
 	 * 
+	 * resolução LOIANE
+	 * 
 	 */
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		Aluno aluno = new Aluno();
+
+		System.out.println("Entre com o nome do aluno");
+		aluno.setNome(scan.next());
+
+		System.out.println("Entre com o nome do curso");
+		aluno.setNomeCurso(scan.next());
+
+		System.out.println("Entre com a matricula");
+		aluno.setMatricula(scan.next());
+
+		// aluno.nomeDisciplinas = new String[3];
+		for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
+			System.out.println("Entre com o nome da disciplina " + i);
+			aluno.setNomeDisciplinaPos(i, scan.next());
+		}
+
+		for (int i = 0; i < aluno.getNotasDisciplinas().length; i++) {
+			System.out.println("Obtendo notas da disciplina " + aluno.getNomeDisciplinas()[i]);
+			for (int j = 0; j < aluno.getNotasDisciplinas()[i].length; j++) {
+				System.out.println("Entre com a nota " + (j + 1));
+				aluno.setNomePosIJ(i, j, scan.nextDouble());
+			}
+		}
+
+		aluno.mostrarInfo();
+
+		for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
+			if (aluno.verificarAprovado(i)) {
+				System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - foi aprovado");
+			} else {
+				System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - reprovado");
+			}
+		}
+	}
+
 }
