50bb20ca4caadda6ff12a3d771f2c7a30b315e71
José Malcher Jr <malcher.malch@gmail.com>
2016-08-17 15:03:48 -0300

    Exerc 01 - Criar um contador, incrementar o seu valor

--- 07 - ORIENTACAO-A-OBJETOS/src/aula34/labs/Exerc01.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula34/labs/Exerc01.java
@@ -7,4 +7,23 @@ public class Exerc01 {
 	 * zerar, incrementar e retornar o valor do contador. Desenvolva um programa
 	 * para testar essa classe.
 	 */
+	
+	static void imprimirValor(){
+		System.out.println(Contador.obterValor());
+	}
+	
+	public static void main(String[] args) {
+		imprimirValor();
+		
+		Contador.incrementar();
+		
+		imprimirValor();
+		
+		Contador.incrementar();
+		Contador.incrementar();
+		Contador.incrementar();
+		
+		imprimirValor();
+		
+	}
 }
