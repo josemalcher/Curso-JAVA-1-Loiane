d859125795b85778e3fc1dc4c96bf71c85587d49
José Malcher Jr <malcher.malch@gmail.com>
2016-08-17 15:58:01 -0300

    Exerc 02 - Calculadora com funções basicas usando métdos staticos

---
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula34/labs/Calculadora.java
@@ -0,0 +1,31 @@
+package aula34.labs;
+
+public class Calculadora {
+
+	public static int soma(int num1,int num2){
+		return num1 + num2;
+	}
+	
+	public static int subtrair(int num1, int num2){
+		return num1 - num2;
+	}
+	
+	public static int multiplicar(int num1, int num2){
+		return num1 * num2;
+	}
+	
+	public static double dividir(int num1, int num2){
+		return num1 / num2;
+	}
+	
+	public static int potencia(int num1, int num2){
+		int total = 1;
+		for(int i = 1 ; i < num2 ; i++){
+			total *= num1;
+		}
+		return total;
+	}
+	
+	
+	
+}
--- 07 - ORIENTACAO-A-OBJETOS/src/aula34/labs/Exerc02.java
+++ 07 - ORIENTACAO-A-OBJETOS/src/aula34/labs/Exerc02.java
@@ -6,4 +6,23 @@ public class Exerc02 {
 	 * subtrair, multiplicar, dividir (dois números), elevar à potência n.
 	 * Desenvolva um programa para testar essa classe.
 	 */
+	
+	static void imprimirTela(int num){
+		System.out.println(num);
+	}
+	static void imprimirTela(double num){
+		System.out.println(num);
+	}
+	
+	public static void main(String[] args) {
+		
+		int soma = Calculadora.soma(1, 10);
+		System.out.println(soma);
+		
+		imprimirTela(Calculadora.dividir(55, 10));
+	
+		
+		
+	}
+	
 }
