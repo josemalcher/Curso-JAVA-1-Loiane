06546051c8800008d5b399e538216b7ce3325fe2
José Malcher Jr <malcher.malch@gmail.com>
2016-08-20 08:36:21 -0300

    Aula 37 Herança

---
+++ 08-OO-HERANCA-INTERFACES-POLIMORFISMO/src/aula37/Aluno.java
@@ -0,0 +1,35 @@
+package aula37;
+
+public class Aluno extends Pessoa{
+
+private String curso;
+private double[] notas;
+
+//Métodos
+public double calcularMedia(){
+	return 0;
+}
+
+public boolean verificarAprovado(){
+	return true;
+}
+
+
+//****GETs SETs
+
+public String getCurso() {
+	return curso;
+}
+public void setCurso(String curso) {
+	this.curso = curso;
+}
+public double[] getNotas() {
+	return notas;
+}
+public void setNotas(double[] notas) {
+	this.notas = notas;
+}
+
+
+
+}
