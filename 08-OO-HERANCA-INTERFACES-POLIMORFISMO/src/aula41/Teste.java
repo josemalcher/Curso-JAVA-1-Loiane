29ae7a63c7b44bb14282a5e0183f1b518bd50a7a
José Malcher Jr <malcher.malch@gmail.com>
2016-08-22 13:03:43 -0300

    Aula 41 - Herança: classes abstratas

---
+++ 08-OO-HERANCA-INTERFACES-POLIMORFISMO/src/aula41/Teste.java
@@ -0,0 +1,20 @@
+package aula41;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		//Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+		
+		//pessoa.setEndereco("Rua 1, n1");
+		aluno.setEndereco("Rua 2, n2");
+		professor.setEndereco("Rua 3, n3");
+		
+		//System.out.println(pessoa.obterEtiquetaEndereco());
+		System.out.println(aluno.obterEtiquetaEndereco());
+		System.out.println(professor.obterEtiquetaEndereco());
+
+	}
+
+}
