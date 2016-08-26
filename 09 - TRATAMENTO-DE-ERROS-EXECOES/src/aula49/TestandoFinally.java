2167b0152041095ab82101038bb7a592f6c9f8bf
José Malcher Jr <malcher.malch@gmail.com>
2016-08-26 16:36:08 -0300

    AUla 49 - Exception - FINALLY e 1 exemplo de pegadinha System.exit(0)

---
+++ 09 - TRATAMENTO-DE-ERROS-EXECOES/src/aula49/TestandoFinally.java
@@ -0,0 +1,21 @@
+package aula49;
+
+public class TestandoFinally {
+	public static void main(String[] args) {
+		int[] numeros = { 4, 8, 16, 32, 64, 128 };
+		int[] demon = { 2, 0, 4, 8, 0 };
+
+		for (int i = 0; i < numeros.length; i++) {
+			try {
+				System.out.println(numeros[i] + "/" + demon[i] + " = " + (numeros[i] / demon[i]));
+			} catch (ArithmeticException e) {
+				System.out.println("Erro ao dividir por zero");
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Posição do array inválidade");
+			}
+			finally {
+				System.out.println("Linha executada ao final do processo - SEMPRE será executado!");
+			}
+		}
+	}
+}
