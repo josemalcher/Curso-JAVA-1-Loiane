c08e3bb762e847357b57331ff8068c5dc65f0a75
José Malcher Jr <malcher.malch@gmail.com>
2016-08-26 15:00:51 -0300

    Exerc 01 02 03 - calculo de figuras geometricas

---
+++ 08-OO-HERANCA-INTERFACES-POLIMORFISMO/src/aula46/labs/Cubo.java
@@ -0,0 +1,25 @@
+package aula46.labs;
+
+public class Cubo extends Figura3D{
+	private double lado;
+
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	@Override
+	public double calcularArea() {
+		return 6 * (lado * lado);
+	}
+
+	@Override
+	public double calcularVolume() {	
+		return lado * lado * lado;
+		// return Math.pow(lado, 3);
+	}
+	
+}
