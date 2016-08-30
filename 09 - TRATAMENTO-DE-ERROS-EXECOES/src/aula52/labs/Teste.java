0882f20b393650f5d7e1e20118313a0eff9b8dfb
José Malcher Jr <malcher.malch@gmail.com>
2016-08-29 21:22:05 -0300

    Exerc 01 - 28 min - Exercício final exception, agenda de contatos

--- 09 - TRATAMENTO-DE-ERROS-EXECOES/src/aula52/labs/Teste.java
+++ 09 - TRATAMENTO-DE-ERROS-EXECOES/src/aula52/labs/Teste.java
@@ -7,31 +7,52 @@ import java.util.Scanner;
  * https://github.com/loiane/curso-java-basico/tree/master/eclipse/curso-java-basico/src/com/loiane/cursojava/aula52/labs
  */
 public class Teste {
-	
-	public int obterOpcaoMenu(Scanner leitor){
-		
-		System.out.println("Digite a opção desejada: ");
-		System.out.println("1: consultar contato");
-		System.out.println("2: adicionar contato ");
-		System.out.println("3: Sair ");
-		
-		int opcao= leitor.nextInt();
-		
+
+	public static int obterOpcaoMenu(Scanner leitor) {
+		boolean entradaValida = false;
+		int opcao = 3;
+		while (!entradaValida) {
+			System.out.println("Digite a opção desejada: ");
+			System.out.println("1: consultar contato");
+			System.out.println("2: adicionar contato ");
+			System.out.println("3: Sair ");
+			
+			try{
+				String entrada = leitor.nextLine();
+				opcao = Integer.parseInt(entrada);
+				if(opcao == 1 || opcao == 2 || opcao == 3){
+					entradaValida = true;
+					
+				}else{
+					throw new Exception("Entrada in´valida \n");
+				}
+			}catch(Exception e){
+				System.out.println("Entrada inválida, digite novamente \n");
+			}
+			
+		}
 		return opcao;
 	}
-	
-	
+
 	public static void main(String[] args) {
-/*		Contato c1 = new Contato();
-		System.out.println(c1);
+		/*
+		 * Contato c1 = new Contato(); System.out.println(c1);
+		 * 
+		 * Contato c2 = new Contato(); System.out.println(c2);
+		 * 
+		 * Contato c3 = new Contato(); System.out.println(c3);
+		 */
 		
-		Contato c2 = new Contato();
-		System.out.println(c2);
+		Scanner leitor = new Scanner(System.in);
+		int opcao = obterOpcaoMenu(leitor);
 		
-		Contato c3 = new Contato();
-		System.out.println(c3);*/
-		
-		
-		
+		if(opcao == 1){
+			
+		}else if(opcao == 2){
+			
+		}else if(opcao ==3){
+			System.exit(0);
+		}
+
 	}
 }
